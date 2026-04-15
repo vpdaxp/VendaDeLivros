a0d09b8b0eab914b29815b545067b305cc6a0aa1
vpdaxp <piresvitord@gmail.com>
2026-04-15 08:35:59 -0300

    Alteração PojoOutputStream e gitignore

--- trabalho P1/VendaDeLivro/io/PojoOutputStream.java
+++ trabalho P1/VendaDeLivro/io/PojoOutputStream.java
@@ -4,6 +4,8 @@ import model.Produto;
 import model.LivroFisico;
 import model.LivroDigital;
 import model.LivroColecionavel;
+
+import java.io.ByteArrayOutputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
@@ -25,33 +27,42 @@ public class PojoOutputStream extends OutputStream {
     }
 
     public void enviarDados() throws IOException {
-        DataOutputStream dos = new DataOutputStream(destino);
+    DataOutputStream dos = new DataOutputStream(destino);
+    dos.writeInt(numObjetos);
 
-        dos.writeInt(numObjetos);
+    for (int i = 0; i < numObjetos; i++) {
+        Produto p = produtos[i];
 
-        for (int i = 0; i < numObjetos; i++) {
-            Produto p = produtos[i];
+        // 1. Criamos um buffer temporário em memória
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        DataOutputStream tempDos = new DataOutputStream(baos);
 
-            if (p instanceof LivroFisico) dos.writeInt(1);
-            else if (p instanceof LivroDigital) dos.writeInt(2);
-            else if (p instanceof LivroColecionavel) dos.writeInt(3);
+        // 2. Escrevemos TUDO que compõe o objeto no buffer temporário
+        tempDos.writeInt(p.getId());
+        tempDos.writeUTF(p.getTitulo());
+        tempDos.writeUTF(p.getDescricao());
+        tempDos.writeUTF(p.getCategoria());
+        tempDos.writeDouble(p.getPreco());
+        tempDos.writeUTF(p.getAutor());
+        enviarDadosEspecificos(tempDos, p); // Também escreve os específicos
+        
+        tempDos.flush();
+        byte[] objetoEmBytes = baos.toByteArray();
 
-            byte[] tituloBytes = p.getTitulo().getBytes();
-            int totalBytesAtributos = 4 + 8 + tituloBytes.length;
+        // 3. Agora enviamos os metadados e o conteúdo real para o destino
+        // Identificador de tipo
+        if (p instanceof LivroFisico) dos.writeInt(1);
+        else if (p instanceof LivroDigital) dos.writeInt(2);
+        else if (p instanceof LivroColecionavel) dos.writeInt(3);
 
-            dos.writeInt(totalBytesAtributos);
-
-            dos.writeInt(p.getId());
-            dos.writeUTF(p.getTitulo());
-            dos.writeUTF(p.getDescricao());
-            dos.writeUTF(p.getCategoria());
-            dos.writeDouble(p.getPreco());
-            dos.writeUTF(p.getAutor());
-
-            enviarDadosEspecificos(dos, p);
-        }
-        dos.flush();
+        // O tamanho exato do que foi escrito no buffer
+        dos.writeInt(objetoEmBytes.length);
+        
+        // Os dados propriamente ditos
+        dos.write(objetoEmBytes);
     }
+    dos.flush();
+}
 
     private void enviarDadosEspecificos(DataOutputStream dos, Produto p) throws IOException {
         if (p instanceof LivroFisico) {
