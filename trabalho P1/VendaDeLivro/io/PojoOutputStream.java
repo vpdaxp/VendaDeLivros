f6f8be63a06056d036cf5b450b58926d404c30b2
vpdaxp <piresvitord@gmail.com>
2026-05-16 19:10:19 -0300

    Alteração PojoOutputStream e gitignore 2

--- trabalho P1/VendaDeLivro/io/PojoOutputStream.java
+++ trabalho P1/VendaDeLivro/io/PojoOutputStream.java
@@ -33,32 +33,30 @@ public class PojoOutputStream extends OutputStream {
     for (int i = 0; i < numObjetos; i++) {
         Produto p = produtos[i];
 
-        // 1. Criamos um buffer temporário em memória
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         DataOutputStream tempDos = new DataOutputStream(baos);
 
-        // 2. Escrevemos TUDO que compõe o objeto no buffer temporário
+        
         tempDos.writeInt(p.getId());
         tempDos.writeUTF(p.getTitulo());
         tempDos.writeUTF(p.getDescricao());
         tempDos.writeUTF(p.getCategoria());
         tempDos.writeDouble(p.getPreco());
         tempDos.writeUTF(p.getAutor());
-        enviarDadosEspecificos(tempDos, p); // Também escreve os específicos
+        enviarDadosEspecificos(tempDos, p);
         
         tempDos.flush();
         byte[] objetoEmBytes = baos.toByteArray();
 
-        // 3. Agora enviamos os metadados e o conteúdo real para o destino
-        // Identificador de tipo
+        
         if (p instanceof LivroFisico) dos.writeInt(1);
         else if (p instanceof LivroDigital) dos.writeInt(2);
         else if (p instanceof LivroColecionavel) dos.writeInt(3);
 
-        // O tamanho exato do que foi escrito no buffer
+        
         dos.writeInt(objetoEmBytes.length);
         
-        // Os dados propriamente ditos
+       
         dos.write(objetoEmBytes);
     }
     dos.flush();
